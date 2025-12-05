529f5567eb8ef60ca0afccac2f9961bd512c6a91
Brian Cooper <brian@brian-cooper.com>
2025-12-04 20:30:11 -0800

    feature: add app config

---
+++ src/lib/config/app.config.ts
@@ -0,0 +1,13 @@
+/**
+ * Application configuration.
+ */
+const app = {
+  name: { short: "Omni", long: "Omni Docs" },
+  organization: { website: "https://omni.dev" },
+  socials: {
+    x: "https://x.com/omnidotdev",
+    github: "https://github.com/omnidotdev",
+  },
+};
+
+export default app;
