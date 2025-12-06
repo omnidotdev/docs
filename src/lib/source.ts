874931147e034e2be62369562f298064ec067693
Brian Cooper <brian@brian-cooper.com>
2025-12-05 22:38:43 -0800

    feature(sidebar): enable icon support

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,6 +1,8 @@
 import { docs } from "fumadocs-mdx:collections/server";
 import { loader } from "fumadocs-core/source";
 import { lucideIconsPlugin } from "fumadocs-core/source/lucide-icons";
+import { createElement } from "react";
+import { icons } from "lucide-react";
 
 // TODO remote MDX sources https://fumadocs.dev/docs/mdx/performance#remote-source
 
@@ -9,6 +11,11 @@ import { lucideIconsPlugin } from "fumadocs-core/source/lucide-icons";
  * @see https://fumadocs.dev/docs/headless/source-api
  */
 const source = loader({
+  icon: (icon) => {
+    if (!icon) return;
+
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+  },
   baseUrl: "/",
   source: docs.toFumadocsSource(),
   // TODO consolidate icon libs, currently using `react-icons` and `lucide-react`
--- src/routes/$/index.tsx
+++ src/routes/$/index.tsx
@@ -67,6 +67,8 @@ const Page = () => {
                     <span className="flex gap-2">
                       {item.icon}
                       {item.name}
+
+                      {/* TODO pillar description */}
                     </span>
                   </p>
                 </div>
@@ -84,7 +86,7 @@ const Page = () => {
                 style={{ paddingInlineStart: "calc(2 * var(--spacing))" }}
                 className="wrap-anywhere active relative flex flex-row items-center justify-between gap-2 rounded-lg p-2 text-start text-fd-muted-foreground transition-colors hover:bg-fd-accent/50 hover:text-fd-accent-foreground/80 hover:transition-none data-[active=true]:bg-fd-primary/10 data-[active=true]:text-fd-primary data-[active=true]:hover:transition-colors [&amp;_svg]:size-4 [&amp;_svg]:shrink-0"
               >
-                <span className="flex gap-2">
+                <span className="flex items-center gap-2">
                   {item.icon}
                   {item.name}
                 </span>
