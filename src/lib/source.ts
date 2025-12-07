1db4c207c33220680b6ddfc4540eeb1699676b86
Brian Cooper <brian@brian-cooper.com>
2025-12-06 19:30:34 -0800

    chore: remove TODO

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -18,7 +18,6 @@ const source = loader({
   },
   baseUrl: "/",
   source: docs.toFumadocsSource(),
-  // TODO consolidate icon libs, currently using `react-icons` and `lucide-react`
   plugins: [lucideIconsPlugin()],
 });
 
