4494fc31618a611777bcb19acc52f5f8739ebc44
Brian Cooper <brian@brian-cooper.com>
2026-01-01 09:34:46 -0800

    chore(sections): set Eden as new

--- src/lib/getSectionDescription.tsx
+++ src/lib/getSectionDescription.tsx
@@ -12,7 +12,7 @@ import { REALMS } from "./sections";
 // then need to figure a way to grab the frontmatter here
 
 // TODO grab from Omni API
-export const NEW_PRODUCTS = ["RDK", "Runa"];
+export const NEW_PRODUCTS = ["Eden", "RDK", "Runa"];
 export const COMING_SOON_PRODUCTS = ["Thornberry"];
 
 // TODO augment section metadata directly (unsure if this is possible with Fumadocs)
